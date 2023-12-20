be586c704b5816b5d7fde1a63637d37c6fcdfe01
its zaki <zakiyounus789@gmail.com>
2023-12-20 21:00:09 +0500

    js practise

---
+++ app.js
@@ -0,0 +1,50 @@
+// let head = document.getElementById("head")
+// head.addEventListener('click', showname)
+// function showname(){
+//     let name = prompt("Enter Name")
+//     head.innerHTML = `Name: ${name}`
+// }
+
+
+// let number = 5.456778
+// let num = number.toFixed()
+// console.log(num);
+
+// let text = "Hello planet earth, you are a great planet.";
+// let result = text.lastIndexOf("great");
+// console.log(result);
+
+// const newdate = new  Date(2018, 11 , 25 , 8 , 55 , 0)
+// console.log(newdate.toLocaleDateString());
+// let date = new Date()
+// let d = date.getDay();
+// console.log(d);
+
+
+
+// function totalarea(val1, val2){
+//     let area = val1 * val2 
+//     return area 
+    
+// }
+// let val1 = prompt("enter width")
+// let val2 = prompt("enter height")
+// let result = totalarea (val1, val2)
+// console.log('the total area is ' , result);
+
+
+// function numbercheck(num){
+//     if(num >0){
+//         return "positive";
+//     }
+//     else if (num < 0){
+//         return "negiive";
+
+//     }
+//     else {
+//         return "zero"
+//     }
+// }
+// let output = numbercheck(0)
+
+// console.log(output);
